8082aaecf07e29c328d60ac166d5325eef8d0892
Rikie Patrick <rikiepatrick@gmail.com>
2023-08-29 13:24:02 +0100

    user and tests added

---
+++ lib/user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, id, name, email, phone_number) -> None:
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+
+    def __eq__(self, other: object) -> bool:
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self) -> str:
+        return f"User({self.id}, {self.name}, {self.email}, {self.phone_number})"
