56c77d50193ece32068c7afedaa769dc5f98ae41
OriginalBinaryHustler <mr.watkin@live.co.uk>
2023-08-30 13:27:05 +0100

    tuesday lstings complete backend 80%

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
-
+from lib.space import *
+from lib.space_repository import *
 # Create a new Flask app
 app = Flask(__name__)
 
@@ -13,8 +14,17 @@ app = Flask(__name__)
 #   ; open http://localhost:5000/index
 @app.route('/', methods=['GET'])
 def get_index():
-    return render_template('index.html')
+    connection = get_flask_database_connection(app)
+    repository = SpaceRepository(connection)
+    spaces = repository.all()
+    return render_template('index.html', spaces=spaces )
 
+@app.route('/listing/<id>', methods=['GET'])
+def get_index_redirect(id):
+    connection = get_flask_database_connection(app)
+    repository = SpaceRepository(connection)
+    space = repository.find(id)
+    return render_template('listing.html', space=space)
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
